5712c62c8e42b3a90594d407cd819acae5929b92
Carlos <hola@cafeconcarlos.com>
2024-05-07 14:59:39 +0200

    bootstrap, icon and  initial route

--- Valencia/src/app/app.routes.ts
+++ Valencia/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+// app.routes.ts
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home' }
+];
